b86efdab4e854da77a84f9a8d67bd11442889f20
TroyMoses <mugabimoses07@gmail.com>
2024-04-08 21:47:37 +0600

    temps

--- coach/views.py
+++ coach/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from administrator.forms import PlayerForm, AdministratorForm, StaffForm, TeamForm, MatchForm, TrainingSessionForm, PlayerTeamForm, PlayerGameForm, InjuryForm, PlayerFeedbackForm1, PlayerFeedbackForm2, StaffFeedbackForm1, StaffFeedbackForm2
 from administrator.models import Player, Administrator, Staff, Team, Match, TrainingSession, PlayerTeam, PlayerGame, Injury, PlayerFeedback1, PlayerFeedback2, StaffFeedback1, StaffFeedback2
+from django.contrib import messages
 
 # Create your views here.
 
@@ -24,103 +25,103 @@ def dashboard(request):
 
 def players(request):
     players = Player.objects.all()
-    return render(request, "players.html", {'players': players})
+    return render(request, "coach/players.html", {'players': players})
 
 def add_player(request):
     form = PlayerForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect(reverse('players'))
+            return redirect(reverse('coach/players'))
     else:
         messages.error(request, "Provided data failed validation") 
-    return render(request, "add_player.html", {'form': form})
+    return render(request, "coach/add_player.html", {'form': form})
 
 def administrators(request):
     administrators = Administrator.objects.all()
-    return render(request, "administrators.html", {'administrators': administrators})
+    return render(request, "coach/administrators.html", {'administrators': administrators})
 
 def staff(request):
     staff = Staff.objects.all()
-    return render(request, "staff.html", {'staff': staff})
+    return render(request, "coach/staff.html", {'staff': staff})
 
 def teams(request):
     teams = Team.objects.all()
-    return render(request, "teams.html", {'teams': teams})
+    return render(request, "coach/teams.html", {'teams': teams})
 
 def add_team(request):
     form = TeamForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect(reverse('teams'))
+            return redirect(reverse('coach/teams'))
     else:
         messages.error(request, "Provided data failed validation") 
-    return render(request, "add_team.html", {'form': form})
+    return render(request, "coach/add_team.html", {'form': form})
 
 def matches(request):
     matches = Match.objects.all()
-    return render(request, "matches.html", {'matches': matches})
+    return render(request, "coach/matches.html", {'matches': matches})
 
 def add_match(request):
     form = MatchForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect(reverse('matches'))
+            return redirect(reverse('coach/matches'))
     else:
         messages.error(request, "Provided data failed validation") 
-    return render(request, "add_match.html", {'form': form})
+    return render(request, "coach/add_match.html", {'form': form})
 
 def sessions(request):
     sessions = TrainingSession.objects.all()
-    return render(request, "sessions.html", {'sessions': sessions})
+    return render(request, "coach/sessions.html", {'sessions': sessions})
 
 def add_session(request):
     form = TrainingSessionForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect(reverse('sessions'))
+            return redirect(reverse('coach/sessions'))
     else:
         messages.error(request, "Provided data failed validation") 
-    return render(request, "add_session.html", {'form': form})
+    return render(request, "coach/add_session.html", {'form': form})
 
 def player_team(request):
     player_team = PlayerTeam.objects.all()
-    return render(request, "player_team.html", {'player_team': player_team})
+    return render(request, "coach/player_team.html", {'player_team': player_team})
 
 def add_player_team(request):
     form = PlayerTeamForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect(reverse('player_team'))
+            return redirect(reverse('coach/player_team'))
     else:
         messages.error(request, "Provided data failed validation") 
-    return render(request, "add_player_team.html", {'form': form})
+    return render(request, "coach/add_player_team.html", {'form': form})
 
 def player_matches(request):
     player_matches = PlayerGame.objects.all()
-    return render(request, "player_matches.html", {'player_matches': player_matches})
+    return render(request, "coach/player_matches.html", {'player_matches': player_matches})
 
 def player_injuries(request):
     player_injuries = Injury.objects.all()
-    return render(request, "player_injuries.html", {'player_injuries': player_injuries})
+    return render(request, "coach/player_injuries.html", {'player_injuries': player_injuries})
 
 def player_feedback_session(request):
     player_feedback_session = PlayerFeedback1.objects.all()
-    return render(request, "player_feedback_session.html", {'player_feedback_session': player_feedback_session})
+    return render(request, "coach/player_feedback_session.html", {'player_feedback_session': player_feedback_session})
 
 def player_feedback_match(request):
     player_feedback_match = PlayerFeedback2.objects.all()
-    return render(request, "player_feedback_match.html", {'player_feedback_match': player_feedback_match})
+    return render(request, "coach/player_feedback_match.html", {'player_feedback_match': player_feedback_match})
 
 def staff_feedback_session(request):
     staff_feedback_session = StaffFeedback1.objects.all()
-    return render(request, "staff_feedback_session.html", {'staff_feedback_session': staff_feedback_session})
+    return render(request, "coach/staff_feedback_session.html", {'staff_feedback_session': staff_feedback_session})
 
 def staff_feedback_match(request):
     staff_feedback_match = StaffFeedback2.objects.all()
-    return render(request, "staff_feedback_match.html", {'staff_feedback_match': staff_feedback_match})
+    return render(request, "coach/staff_feedback_match.html", {'staff_feedback_match': staff_feedback_match})
 
