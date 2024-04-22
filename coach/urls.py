5939a249bf870de71a72d31de2c5206adbcdb2ce
TroyMoses <mugabimoses07@gmail.com>
2024-04-22 15:51:57 +0600

    url

--- coach/urls.py
+++ coach/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    # path('login/', views.login, name='login'),
+    # path('register/', views.register, name='register'),
     path('lock/', views.lock, name='register'),
     path('dep/', views.dep, name='dep'),
     path('',views.coachDashboard,name='coachDashboard'),
